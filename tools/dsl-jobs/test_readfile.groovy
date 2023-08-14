94fb7afa12476c07420a1a6c91a5748074ef6d3a
Divi-Tiru-Sai-Gopala-Pavana-Phaneendra <divi.tiru.sai.gopala.pavana.phaneendra@ibm.com>
2023-08-14 13:32:14 +0530

    Testing for cron

--- tools/dsl-jobs/test_readfile.groovy
+++ tools/dsl-jobs/test_readfile.groovy
@@ -1,8 +1,6 @@
-def choicesString = readFileFromWorkspace('options.txt').replace('\n', ',')
+def choicesString = readFileFromWorkspace('options.txt').trim().replace('\n', ',')
 println choicesString
-def cronString = '''TZ=America/Toronto\n
-H */6 * * * %OPTION=$choicesString
-'''
+def cronString = "TZ=America/Toronto\n H */6 * * * %OPTION=" + choicesString
 println cronString
 
 
