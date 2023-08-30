6cc13051560ce6abdd5aaadccfdb54b913d5a529
Divi-Tiru-Sai-Gopala-Pavana-Phaneendra <divi.tiru.sai.gopala.pavana.phaneendra@ibm.com>
2023-08-30 07:50:29 +0530

    Testing cron string

--- tools/dsl-jobs/test_readfile.groovy
+++ tools/dsl-jobs/test_readfile.groovy
@@ -1,7 +1,7 @@
 def choicesString = readFileFromWorkspace('options.txt')
 def choices = choicesString.split('\n').collect { "$it" }
-def cron = "TZ=America/Toronto\n
-40 2 * * 1-4 %OPTION=option_1\n"
+def cron = '''TZ=America/Toronto\n
+40 2 * * 1-4 %OPTION=option_1'''
 pipelineJob("Testing the reading of a file for parameter options") {
     properties {
         githubProjectUrl('git@github.com:PhaniDivi-613/Test-Jenkins.git')
