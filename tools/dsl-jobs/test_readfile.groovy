a208ee559895e49f3230503b4b47a73ea3e0cc5c
Divi-Tiru-Sai-Gopala-Pavana-Phaneendra <divi.tiru.sai.gopala.pavana.phaneendra@ibm.com>
2023-08-31 14:42:00 +0530

    cron removed

--- tools/dsl-jobs/test_readfile.groovy
+++ tools/dsl-jobs/test_readfile.groovy
@@ -2,12 +2,11 @@ import groovy.json.JsonSlurper
 
 def choicesString = readFileFromWorkspace('options.txt')
 def choices = choicesString.split('\n').collect { "$it" }
-def wd = pwd()
-println(wd)
+
 def jsonSlurper = new JsonSlurper()
 def services = ["atracker", "metrics-router"]
 def cron = "TZ=America/Toronto\n\n"
-def cronTimings = jsonSlurper.parse(readFileFromWorkspace('promotion-cron-timings.json'))
+def cronTimings = readJSON file: 'promotion-cron-timings.json'
 println(cronTimings)
 for (service in services) {
     cronTimings["$service"].each{ region, cronExp ->
