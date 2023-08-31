1a73bdb8b0cacffdbc06aa0f3c130361d253a656
Divi-Tiru-Sai-Gopala-Pavana-Phaneendra <divi.tiru.sai.gopala.pavana.phaneendra@ibm.com>
2023-08-31 14:43:21 +0530

    cron removed

--- tools/dsl-jobs/test_readfile.groovy
+++ tools/dsl-jobs/test_readfile.groovy
@@ -6,7 +6,8 @@ def choices = choicesString.split('\n').collect { "$it" }
 def jsonSlurper = new JsonSlurper()
 def services = ["atracker", "metrics-router"]
 def cron = "TZ=America/Toronto\n\n"
-def cronTimings = readJSON file: 'promotion-cron-timings.json'
+println(readFileFromWorkspace('promotion-cron-timings.json'))
+def cronTimings = jsonSlurper.parse(readFileFromWorkspace('promotion-cron-timings.json'))
 println(cronTimings)
 for (service in services) {
     cronTimings["$service"].each{ region, cronExp ->
