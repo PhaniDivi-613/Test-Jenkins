dd53ba449924d188695cb16e6664ede12c06e931
Divi-Tiru-Sai-Gopala-Pavana-Phaneendra <divi.tiru.sai.gopala.pavana.phaneendra@ibm.com>
2023-12-12 15:21:37 +0530

    Code freeze testing

---
+++ .history/tools/jenkins-jobs/test-code-freeze_20231212152126.groovy
@@ -0,0 +1,60 @@
+import groovy.json.JsonSlurper
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+
+boolean isInCodeFreeze(String region) {
+    def resourcePath = this.getClass().getResource("/codefreeze-timings.json")
+    if (resourcePath) {
+        jsonData = resourcePath.text
+    } else {
+        println "Resource file 'codefreeze-timings.json' not found or inaccessible"
+        // Handle the case where the resource file is not accessible or doesn't exist
+        // For example, you might want to set a default value for jsonData or take alternative actions.
+    }
+    def freezeData = new JsonSlurper().parseText(jsonData)
+
+    def currentDateTime = ZonedDateTime.now()
+    def inFreeze = freezeData.find { event ->
+        def freezeStart = ZonedDateTime.parse(event."Freeze Start")
+        def freezeEnd = ZonedDateTime.parse(event."Freeze End")
+        def regions = event."Regions"
+
+        currentDateTime.isAfter(freezeStart) &&
+        currentDateTime.isBefore(freezeEnd) &&
+        regions.contains(region)
+    }
+
+    return inFreeze != null
+}
+
+pipeline {
+    agent {
+        label 'agent-1'
+    }
+    environment {
+        REGION = "${env.REGION}"
+    }
+    stages {
+        stage('Halt Job based on Codefreeze') {
+            when {
+                expression {
+                    isInCodeFreeze(env.REGION)
+                }
+            }
+            steps {
+                script {
+                    currentBuild.result = 'ABORTED'
+                    error "Job halted due to code freeze in the specified region (${env.REGION})"
+                }
+            }
+        }
+        stage('Stage 2') {
+            steps {
+                script {
+                    echo "Stage 2 is executing"
+                    // Your Stage 2 steps here
+                }
+            }
+        }
+    }
+}
