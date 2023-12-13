9ff997e993319e97debc5490859ba386c5540eb7
Divi-Tiru-Sai-Gopala-Pavana-Phaneendra <divi.tiru.sai.gopala.pavana.phaneendra@ibm.com>
2023-12-13 16:35:34 +0530

    code freeze testing

--- tools/jenkins-jobs/test-code-freeze.groovy
+++ tools/jenkins-jobs/test-code-freeze.groovy
@@ -1,3 +1,32 @@
+import groovy.json.JsonSlurper
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.time.ZoneId
+
+boolean isInCodeFreeze(String region) {
+    def jsonData = readFile 'tools/codefreeze-timings.json'
+    def freezeData = new groovy.json.JsonSlurper().parseText(jsonData)
+
+    def formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")
+    def currentDateTime = ZonedDateTime.now(ZoneId.of('UTC'))
+
+    def inFreeze = freezeData.find { event ->
+        def freezeStart = ZonedDateTime.parse(event."Freeze Start").withZoneSameInstant(ZoneId.of("UTC"))
+        def freezeEnd = ZonedDateTime.parse(event."Freeze End").withZoneSameInstant(ZoneId.of("UTC"))
+        def regions = event."Regions"
+
+        println "Freeze Start: ${freezeStart}"
+        println "Current DateTime: ${currentDateTime}"
+        println "Freeze End: ${freezeEnd}"
+        println "Regions: ${regions}"
+
+        currentDateTime.isAfter(freezeStart) && currentDateTime.isBefore(freezeEnd) && regions.contains(region)
+    }
+
+    println "In Freeze: ${inFreeze}"
+    return inFreeze != null
+}
+
 pipeline { 
     agent {
         label 'agent-1'
