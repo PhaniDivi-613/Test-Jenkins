2f90a1ea77e027e148ac3d04789ace2d7e7208ac
Divi-Tiru-Sai-Gopala-Pavana-Phaneendra <divi.tiru.sai.gopala.pavana.phaneendra@ibm.com>
2023-12-13 14:04:12 +0530

    code freeze testing

--- tools/jenkins-jobs/test-code-freeze.groovy
+++ tools/jenkins-jobs/test-code-freeze.groovy
@@ -30,7 +30,8 @@ node('agent-1') {
     environment{
         REGION = "${env.DEPLOYMENT.split('_')[1]}"
     }
-    print(${env.REGION})
+    print(env.REGION)
+    print(REGION)
 
     if (isInCodeFreeze(${env.REGION})) {
         echo "Code freeze detected in the specified region (${REGION}). Skipping the job."
