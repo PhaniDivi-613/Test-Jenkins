29d408318f5a437b2de56123f7d0206523b6df51
Divi-Tiru-Sai-Gopala-Pavana-Phaneendra <divi.tiru.sai.gopala.pavana.phaneendra@ibm.com>
2023-12-13 13:49:09 +0530

    active test job added

---
+++ tools/dsl-jobs/folder-1/active_choice_test.groovy
@@ -0,0 +1,57 @@
+pipelineJob('ExampleDSLJob') {
+    description('This is an example DSL job')
+
+    parameters {
+        // Define parameters for the job
+        stringParam('USERNAME', '', 'Enter your username')
+        password('PASSWORD', '', 'Enter your password')
+        booleanParam('ENABLE_FEATURE', true, 'Enable feature')
+        choice(name: 'CHOICE_PARAM', choices: ['Option A', 'Option B', 'Option C'], description: 'Choose an option')
+        // Active Choices parameter
+        [$class: 'CascadeChoiceParameter', 
+            choiceType: 'PT_SINGLE_SELECT', 
+            description: 'Select an option', 
+            filterLength: 1, 
+            filterable: false, 
+            name: 'DynamicChoices', 
+            referencedParameters: '', 
+            script: [
+                $class: 'GroovyScript', 
+                fallbackScript: 'return ["Error: Unable to fetch data"]', 
+                script: [
+                    $class: 'GroovyScript', 
+                    script: 'return calculateChoices()', // Call the method to generate the list
+                ],
+            ],
+        ]
+    }
+
+    definition {
+        cps {
+            script("""
+                // Your pipeline script here
+                node {
+                    stage('Example Stage') {
+                        echo "Hello, Jenkins DSL!"
+                        // Use parameters in your pipeline
+                        echo "Username: \${params.USERNAME}"
+                        echo "Feature enabled: \${params.ENABLE_FEATURE}"
+                        echo "Chosen option: \${params.CHOICE_PARAM}"
+                        // Use the Active Choices parameter
+                        echo "Dynamic choice: \${params.DynamicChoices}"
+                    }
+                }
+            """)
+        }
+    }
+}
+
+// Method to generate choices dynamically
+def calculateChoices() {
+    def list = [] // Initialize an empty list
+    // Generate values dynamically (replace this with your logic)
+    for (int i = 1; i <= 10; i++) {
+        list.add("Option ${i}") // Add options to the list
+    }
+    return list // Return the generated list
+}
