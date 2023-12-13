4d1fa7888750e3c5fd8681fdb227efd12fd9e675
Divi-Tiru-Sai-Gopala-Pavana-Phaneendra <divi.tiru.sai.gopala.pavana.phaneendra@ibm.com>
2023-12-13 16:33:07 +0530

    code freeze testing

--- tools/jenkins-jobs/test-code-freeze.groovy
+++ tools/jenkins-jobs/test-code-freeze.groovy
@@ -1,49 +1,38 @@
-import groovy.json.JsonSlurper
-import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
-import java.time.ZoneId
-
-boolean isInCodeFreeze(String region) {
-    def jsonData = readFile 'tools/codefreeze-timings.json'
-    def freezeData = new groovy.json.JsonSlurper().parseText(jsonData)
-
-    def formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")
-    def currentDateTime = ZonedDateTime.now(ZoneId.of('UTC'))
-
-    def inFreeze = freezeData.find { event ->
-        def freezeStart = ZonedDateTime.parse(event."Freeze Start").withZoneSameInstant(ZoneId.of("UTC"))
-        def freezeEnd = ZonedDateTime.parse(event."Freeze End").withZoneSameInstant(ZoneId.of("UTC"))
-        def regions = event."Regions"
-
-        currentDateTime.isAfter(freezeStart) && currentDateTime.isBefore(freezeEnd) && regions.contains(region)
-    }
-
-    println "In Freeze: ${inFreeze}"
-    return inFreeze != null
-}
 pipeline { 
     agent {
         label 'agent-1'
     }
-    environment{
+    environment {
         REGION = "${env.DEPLOYMENT.split('_')[1]}"
     }
-    stages{
-        stage('List All Files') {
-            when {
-                expression {
-                    isInCodeFreeze(REGION)
-                } 
+    stages {
+        stage('Check Code Freeze') {
+            steps {
+                script {
+                    if(currentBuild.rawBuild.getCause(hudson.model.Cause$UserIdCause)) {
+                        echo "Triggered by a user"
+                    } else if(currentBuild.rawBuild.getCause(hudson.triggers.TimerTrigger$TimerTriggerCause)) {
+                        echo "Triggered by a timer/cron"
+                    }
+                    def inCodeFreeze = isInCodeFreeze(env.REGION)
+                    if (inCodeFreeze) {
+                        error "Code freeze is active for ${env.REGION}. Halting the job."
+                    } else {
+                        echo "No code freeze active for ${env.REGION}. Continuing the job."
+                    }
+                }
             }
-            steps{
+        }
+        stage('List All Files') {
+            steps {
                 script {
                     sh 'cd . && ls -la /'
                     sh 'find . -name "codefreeze-timings.json"'
                 }
             }
         }
-        stage('simple stage') {
-            steps{
+        stage('Simple Stage') {
+            steps {
                 script {
                     sh 'echo "phani"'
                 }
