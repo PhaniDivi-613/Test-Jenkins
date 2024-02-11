cb59d1fa713c3948367f097a680bc7cc01a35b44
Divi-Tiru-Sai-Gopala-Pavana-Phaneendra <divi.tiru.sai.gopala.pavana.phaneendra@ibm.com>
2024-02-11 22:49:41 +0530

    testing code freeze

--- tools/jenkins-jobs/test-code-freeze.groovy
+++ tools/jenkins-jobs/test-code-freeze.groovy
@@ -10,33 +10,15 @@ pipeline {
         stage('Check Code Freeze') {
             steps {
                 script {
-                    // def isCronTrigger = currentBuild.rawBuild.getCause(hudson.triggers.TimerTrigger$TimerTriggerCause) != null
-                    // def codeFreezeActive = isInCodeFreeze(env.LOCATION)
-
-                    // if (codeFreezeActive && isCronTrigger) {
-                    //     error "Code freeze is active for ${env.LOCATION} and job triggered by cron. Halting the job."
-                    // } else if (codeFreezeActive && !isCronTrigger) {
-                    //     def userInput = input(
-                    //         id: 'codeFreezeConfirmation',
-                    //         message: "Code freeze is active for ${env.LOCATION}. Do you want to proceed?",
-                    //         parameters: [choice(choices: ['Yes', 'No'], description: 'Choose whether to proceed', name: 'Confirmation')]
-                    //     )
-                        
-                    //     if (userInput == 'No') {
-                    //         error "User opted not to proceed during code freeze. Halting the job."
-                    //     } else {
-                    //         echo "Continuing the job despite code freeze."
-                    //     }
-                    // } else {
-                    //     echo "No code freeze active for ${env.LOCATION}. Continuing the job."
-                    // }
 
                     sh """
 
-                        echo "BUILD_CAUSE_USERIDCAUSE: ${BUILD_CAUSE_USERIDCAUSE}"
-                        echo "BUILD_CAUSE_SCMTRIGGER: ${BUILD_CAUSE_SCMTRIGGER}"
-                        echo "BUILD_CAUSE_UPSTREAMTRIGGER: ${BUILD_CAUSE_UPSTREAMTRIGGER}"
-                        echo "BUILD_CAUSE_MANUALTRIGGER: ${BUILD_CAUSE_MANUALTRIGGER}"
+                        // echo "BUILD_CAUSE_USERIDCAUSE: ${BUILD_CAUSE_USERIDCAUSE}"
+                        // echo "BUILD_CAUSE_SCMTRIGGER: ${BUILD_CAUSE_SCMTRIGGER}"
+                        // echo "BUILD_CAUSE_UPSTREAMTRIGGER: ${BUILD_CAUSE_UPSTREAMTRIGGER}"
+                        // echo "BUILD_CAUSE_MANUALTRIGGER: ${BUILD_CAUSE_MANUALTRIGGER}"
+                        echo "CAUSE: ${CAUSE}"
+                        echo "BUILD_USER: ${BUILD_USER}"
 
                         cd scripts
                         python3 code-freeze.py
