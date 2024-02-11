48adba8c98bec0dc0c6527a9810bdebe68712333
Divi-Tiru-Sai-Gopala-Pavana-Phaneendra <divi.tiru.sai.gopala.pavana.phaneendra@ibm.com>
2024-02-11 22:50:20 +0530

    testing code freeze

--- tools/jenkins-jobs/test-code-freeze.groovy
+++ tools/jenkins-jobs/test-code-freeze.groovy
@@ -12,11 +12,6 @@ pipeline {
                 script {
 
                     sh """
-
-                        // echo "BUILD_CAUSE_USERIDCAUSE: ${BUILD_CAUSE_USERIDCAUSE}"
-                        // echo "BUILD_CAUSE_SCMTRIGGER: ${BUILD_CAUSE_SCMTRIGGER}"
-                        // echo "BUILD_CAUSE_UPSTREAMTRIGGER: ${BUILD_CAUSE_UPSTREAMTRIGGER}"
-                        // echo "BUILD_CAUSE_MANUALTRIGGER: ${BUILD_CAUSE_MANUALTRIGGER}"
                         echo "CAUSE: ${CAUSE}"
                         echo "BUILD_USER: ${BUILD_USER}"
 
