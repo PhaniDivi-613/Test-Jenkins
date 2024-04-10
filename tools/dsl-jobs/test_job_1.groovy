71ce034779c69738bf1a5002863c59f46ad81c95
Divi-Tiru-Sai-Gopala-Pavana-Phaneendra <divi.tiru.sai.gopala.pavana.phaneendra@ibm.com>
2024-04-10 19:21:20 +0530

    Testing cIs

--- tools/dsl-jobs/test_job_1.groovy
+++ tools/dsl-jobs/test_job_1.groovy
@@ -22,31 +22,6 @@ pipelineJob("Testing Job 1") {
     }
     parameters {
         stringParam('TRAIN_ID', '', 'Leave it blank for dev environment. Will be used in stage/prod environment only. <br>If you already have a valid train id, put it here and the job will skip Train request/approval.' )
-        stringParam {
-            name('ARTIFACTORY_DOCKERHUB')
-            defaultValue('docker-na-public.artifactory.swg-devops.com/wcp-argonauts-kirkman-team-observability-docker-virtual')
-            description('Name of the Artifactory from where the image will be pulled')
-            trim(true)
-        }
-        stringParam {
-            name('DOCKER_IMAGE')
-            defaultValue('ubi8/ubi')
-            description('Docker Image Name (e.g ubuntu or ubi8/ubi)')
-            trim(true)
-        }
-        stringParam {
-            name('DOCKER_TAG')
-            defaultValue('8.3')
-            description('Docker Image Tag (e.g latest)')
-            trim(true)
-        }
-        stringParam {
-            name('REGISTRY')
-            defaultValue('us.icr.io')
-            description('This is the target container registry (e.g us.icr.io)')
-            trim(true)
-        }
-        stringParam('REGISTRY_NAMESPACE', 'atracker-prod', 'This is the target namespace (e.g atracker-prod)')
     }
     description()
     keepDependencies(false)
